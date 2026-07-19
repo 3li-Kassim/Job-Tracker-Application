86306b5d73c5c8de6a97e2f7ff12f958bcd93880
Ali <24805497@stu.mmu.ac.uk>
2026-07-19 23:49:03 +0100

    finished the modal, added all the forms.

--- Frontend/src/Components/modal.jsx
+++ Frontend/src/Components/modal.jsx
@@ -4,8 +4,27 @@ import { useState } from 'react';
 
 export function ModalComp(props){
 
-    const [dateApplied, setDateApplied] = useState(null);
-    const [input, setInput] = useState();
+    
+
+    const [formData, setFormData] = useState({
+            company: '',
+            role: '',
+            location:'',
+            jobLink:'',
+            status:'',
+            result:'',
+            dateApplied:'',
+            feedbackDate:''
+
+        });
+
+    const handleInputChange = (e) => {
+        const {name , value} = e.target;
+        setFormData(prevData =>({
+            ...prevData,
+            [name]: value
+        }));
+    }
 
     return(
         <div className="modal show" style={{display: 'block', position:'initial'}}>
@@ -15,32 +34,55 @@ export function ModalComp(props){
                 </Modal.Header>
                 <Modal.Body>
                     <form>
+                        <div className="mb-3">
                         <label>Company</label>
-                        <input className="form-control" type="text" name="company"/>
+                        <input className="form-control" type="text" name="company" value={formData.company} onChange={handleInputChange} />
+                        </div>
+                        
+                        <div className="mb-3">
                         <label>Role</label>
-                        <input className="form-control" type="text" name="role"/>
+                        <input className="form-control" type="text" name="role" value={formData.role} onChange={handleInputChange}/>
+                        </div>
+                        
+                        <div className="mb-3">
                         <label>Location</label>
-                        <input className="form-control" type="text" name="location"/>
+                        <input className="form-control" type="text" name="location" value={formData.location} onChange={handleInputChange}/>
+                        </div>
+                        
+                        <div className="mb-3">
                         <label>Job link</label>
-                        <input className="form-control" type="text" name="job-link"/>
+                        <input className="form-control" type="text" name="jobLink" value={formData.jobLink} onChange={handleInputChange}/>
+                        </div>
+                        
+                        <div className="mb-3">
                         <label>Date Applied</label>
-                        <input type="date" className="form-control" name="date-applied"/>
+                        <input type="date" className="form-control" name="dateApplied" value={formData.dateApplied} onChange={handleInputChange}/>
+                        </div>
+
+                        <div className="mb-3">
                         <label>Status</label>
-                        <select name="status" className="form-select">
+                        <select name="status" className="form-select" value={formData.status} onChange={handleInputChange}>
                             <option value="0" style={{display:'none'}}></option>
                             <option value="not-applied">Not Applied</option>
                             <option value="Applied">Applied</option>
                         </select>
+                        </div>
+                        
+                        <div className="mb-3">
                         <label>Result</label>
-                        <select name="result" className="form-select">
-                            <option value="0" disabled style={{display:'none'}}></option>
+                        <select name="result" className="form-select" value={formData.result} onChange={handleInputChange}>
+                            <option value="" disabled style={{display:'none'}}></option>
                             <option value="no-response">No response</option>
                             <option value="accepted">Accepted</option>
                             <option value="rejected">Rejected</option>
                         </select>
-                        <label >Feedback Date</label>
-                        <input className='form-control' type="date" name="feedback-date"/>
+                        </div>
 
+                        <div className="mb-3">
+                        <label >Feedback Date</label>
+                        <input className='form-control' type="date" name="feedbackDate" value={formData.feedbackDate} onChange={handleInputChange}/>
+                        </div>
+                        
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
